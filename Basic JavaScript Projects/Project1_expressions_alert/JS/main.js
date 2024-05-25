bb27e5c33af3836a57f352688487d9f8083ab499
Kobby236 <edwinaddai.ea@gmail.com>
2024-05-25 17:07:33 -0400

    updated JS code
    Added an alert and comment code

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+
+var A = "This is a string";
+var B = "This is another string" + " that I am concating.\n";
+var dad = "father", mom = "mother", bro = "brother", sis = "sister";
+var AB = A + " and " + B; // Variable AB is concatenating the value of var A and B 
+
+3 + 3 // expression
+
+window.alert(A)
+document.write(B)
+document.write(bro)
+document.write(AB)
+
+
+
