0791f5b475778ecd423784a76626695ce7640b8d
Kobby236 <edwinaddai.ea@gmail.com>
2024-05-25 17:59:09 -0400

    javascript function
    Added JavaScript function to my code.

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,7 @@
+function Btn_Function () {
+    var text1 = "This is the first click event in JavaScript"; // assigning var text1 with a string
+    text1 += "This is the second click event in JavaScript"; // concatenating text1 using operator
+
+    document.getElementById("para").innerHTML = text1; // modifying the HTML element with an Id para
+
+}
